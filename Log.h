24f5593d2b9689b723885a998585e271a5cee814
Arman <ovakimian.al@phystech.edu>
2023-09-20 13:05:58 +0300

    Log.h v2 changed LOG macro

--- Log.h
+++ Log.h
@@ -13,20 +13,22 @@ void LogClose();
 
 //TODO: __DATE__ and __TIME__ is showing compiling time - how to get current time 
 
-#define LOG_START(STRING) fprintf(LOG_FILE, "New log called %s at %s from"                        \
-                                            "file: %s, from function: %s, from line: %d\n\n\n,",  \
-                                            __DATE__, __TIME__, __FILE__, __func__, __LINE__ );
+#define LOG_BEGIN() fprintf(LOG_FILE, "-----------------------\n"                               \
+                                      "New log called %s at %s "                                \
+                                      "from file: %s, from function: %s, from line: %d\n\n\n",  \
+                                      __DATE__, __TIME__, __FILE__, __func__, __LINE__);
 
+#define LOG(...) fprintf(LOG_FILE, __VA_ARGS__);
 
-#define LOG(STRING) fprintf(LOG_FILE, STRING "\n");
-
-#define LOG_END(STRING) fprintf(LOG_FILE, "Logging ended %s at %s in"                \
-                                          "file: %s, function: %s, line: %d\n\n\n,"  \
-                                          __DATE__, __TIME__, __FILE__, __func__, __LINE__ );                                          )
+#define LOG_END() fprintf(LOG_FILE, "-----------------------"                   \
+                                    "Logging ended %s at %s "                   \
+                                    "in file: %s, function: %s, line: %d\n\n\n" \
+                                    __DATE__, __TIME__, __FILE__, __func__, __LINE__);                                          )
 
 static inline void PrintSeparator()
 {
-    fprintf(LOG_FILE, "\n\n---------------------------------------------------------------------------\n\n");
+    fprintf(LOG_FILE,
+            "\n\n---------------------------------------------------------------------------\n\n");
 }
 
 #endif
