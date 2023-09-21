ea12402aca65af62c9da62e197ddac1f02400967
Arman <ovakimian.al@phystech.edu>
2023-09-21 09:21:57 +0300

    Types file to work with stack

--- Types.h
+++ Types.h
@@ -1,2 +1,11 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <math.h>
+
 typedef double ElemType;
-#define ElemTypePrint "%lf"
+#define ElemTypeFormat "%lf"
+
+static const ElemType POISON = NAN;
+
+#endif // TYPES_H
